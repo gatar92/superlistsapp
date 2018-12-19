95f820dcdbfa196e510ec2c49f086bdc352f662b
Pawel <gatar92@gmail.com>
2018-12-19 15:48:20 +0100

    Redirects after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrive_it_later(self):
         #Edith has heard about a cool new online to-do app. She goes to check out its homepage
         self.browser.get('http://localhost:8000')
@@ -35,29 +41,25 @@ class NewVisitorTest(unittest.TestCase):
         #"1. Buy peacock feathers" as an item in a to-do lists
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feathers' for row in rows),
-        #     "New to-do item did not appear in table. Contents were: \n{table.text}"
-        # )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
         #There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        inputbox = se;f.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(10)
         #self.fail('Finish the test!')
 
         #The page updates again, and now shows both items on ger lists
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertIn('1: Buy peacick feathers', [row.text for row in rows])
-        self.assertIn(
-            '2 Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
+        #table = self.browser.find_element_by_id('id_list_table')
+        #rows = table.find_element_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        #self.assertIn('1: Buy peacick feathers', [row.text for row in rows])
+        #self.assertIn(
+        #    '2 Use peacock feathers to make a fly',
+        #    [row.text for row in rows]
+        #)
 
         #Edith wonders wheater the site will remember ger list. Then she sees that the site has generated a unique URL for her --
         #there is some explanatory text to that effect.
